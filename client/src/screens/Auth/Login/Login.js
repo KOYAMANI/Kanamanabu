1e23a7df9aac46be0a135e00bbf5dc53c30dbae0
Paul Ebert <paul.ebert@code.berlin>
2022-01-20 21:04:34 +0100

    feature: added styling for loginscreen

--- client/src/screens/Auth/Login/Login.js
+++ client/src/screens/Auth/Login/Login.js
@@ -46,38 +46,33 @@ const Login = () => {
         <LoginContainer>
           {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
           {loading && <Loading />}
-          <Form onSubmit={submitHandler}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                value={email}
-                placeholder="Enter email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
+          <form onSubmit={submitHandler}>
+
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
   
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                value={password}
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-  
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+            <LoginButton text="Submit" type="submit"></LoginButton>
+
+          </form>
           <Row className="py-3">
             <Col>
               Don't have an account ? <Link to="/register">Register Here</Link>
             </Col>
           </Row>
 
-          <LoginButton text="test"></LoginButton>
         </LoginContainer>
         
       </OnboardingScreen>
