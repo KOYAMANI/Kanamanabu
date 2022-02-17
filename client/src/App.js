54ea8e86ac839387a0e258e5880a200b6ca30dea
KOYAMANI <ykkg3ym1114@gmail.com>
2022-02-17 14:01:57 +0100

    fix path error

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,8 @@ import Register from './screens/Auth/Register/Register';
 import Login from './screens/Auth/Login/Login';
 import Contents from './screens/Contents/Contents'
 import EditProfile from './screens/EditProfile/EditProfile';
-
-import QuizSelection from './screens/Quiz/QuizCategory/QuizCategory';
+import QuizCategory from './screens/Quiz/QuizCategory/QuizCategory'
+import QuizSelection from './screens/Quiz/QuizSelection/QuizSelection';
 import QuizScreen from './screens/Quiz/Quizes/QuizScreen';
 import SignInPage from "./screens/Auth/SignInPage/SignInPage";
 
@@ -29,8 +29,8 @@ const App = () =>(
       <Route path ='/contents' element ={<Contents />} />
       <Route path ='/account' element ={<Account />} />
       <Route path ='/editprofile' element ={<EditProfile />} />
-
-      <Route path ='/quizcategory' element ={<QuizSelection />} />
+      <Route path ='/quizcategory' element ={<QuizCategory />} />
+        <Route path ='/quizselection' element ={<QuizSelection />} />
       <Route path ='/quiz' element={<QuizScreen />} />
 
 
